21ecd05f813a9ee75940936897069afa82fe52aa
shaniblau <blaushani02@gmail.com>
2023-08-09 18:30:07 +0300

    added ci-cd

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ import time
 from datetime import datetime
 from queue import Queue
 
-from multiprocessing import Process
 from multiprocessing.pool import Pool
 from threading import Thread
 
@@ -20,13 +19,24 @@ date = datetime.now().strftime("%d_%m_%Y")
 arrived_logger = extendable_logger(f'../logs/files-arrived/{date}.log', log.INFO)
 
 
+def process_queue(q):
+    while True:
+        if not q.empty():
+            event = q.get()
+            arrived_logger.info(f'the file {event.src_path} has been received')
+            file_name: str = event.src_path.split("/")[-1]
+            file_full_name = file_name.split("_")[0].split('.')[0]
+            if "_a" == file_name[-2:] and "_b" == file_name[-2:]:
+                error_logger.error(f'the file {file_name} name is not in the requested format')
+            else:
+                handle_file(file_name, file_full_name)
+
+
 def run():
     observer = Observer()
     watchdog_queue = Queue()
     handle_existing_files(watchdog_queue)
-    with Pool() as pool:
-        pass
-    worker = Thread(target=start_multi_processing(watchdog_queue), args=(watchdog_queue,), daemon=True)
+    worker = Thread(target=process_queue, args=(watchdog_queue,), daemon=True)
     worker.start()
     event_handler = Handler(watchdog_queue)
     observer.schedule(event_handler, config.IMAGES_DIR_PATH)
@@ -54,18 +64,6 @@ def start_multi_processing(q):
             error_logger.error(f'the files were not sent do to: {Exception.__str__}')
 
 
-def process_queue(q):
-    if not q.empty():
-        event = q.get()
-        arrived_logger.info(f'the file {event.src_path} has been received')
-        half_file_name: str = event.src_path.split("/")[-1].split('.')
-        if "_a" != half_file_name[0][-2:] and "_b" != half_file_name[0][-2:]:
-            error_logger.error(f'the file {half_file_name} name is not in the requested format')
-        else:
-            whole_file_name = half_file_name.split("_")[0]
-            handle_file(half_file_name, whole_file_name)
-
-
 class Handler(FileSystemEventHandler):
     def __init__(self, queue):
         self.queue = queue
@@ -74,14 +72,14 @@ class Handler(FileSystemEventHandler):
         self.queue.put(event)
 
 
-def handle_file(half_file_name, whole_file_name):
-    if Redis.check_existence(whole_file_name):
-        first_half = Redis.extract(whole_file_name)
-        second_half = half_file_name
+def handle_file(file_name, file_full_name):
+    if Redis.check_existence(file_full_name):
+        first_half = Redis.extract(file_full_name)
+        second_half = file_name
         if second_half != first_half:
             HTTPLoad.execute([first_half, second_half])
         else:
-            error_logger.warning(f'the file {half_file_name} has been sent twice')
-            Redis.load(half_file_name, whole_file_name)
+            error_logger.warning(f'the file {file_name} has been sent twice')
+            Redis.load(file_name, file_full_name)
     else:
-        Redis.load(half_file_name, whole_file_name)
+        Redis.load(file_name, file_full_name)
