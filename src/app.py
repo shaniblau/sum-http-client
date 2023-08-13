4a37a02444d607e8e5c14868edc3fd48cc143c18
shaniblau <blaushani02@gmail.com>
2023-08-13 10:51:05 +0300

    tried converting from thread to process

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 import logging as log
+from multiprocessing import Queue
 import os
 import time
 from datetime import datetime
 from multiprocessing import Process
-from queue import Queue
+
 
 from multiprocessing.pool import Pool
 from threading import Thread
@@ -20,24 +21,11 @@ date = datetime.now().strftime("%d_%m_%Y")
 arrived_logger = extendable_logger(f'../logs/files-arrived/{date}.log', log.INFO)
 
 
-def process_queue(q):
-    while True:
-        if not q.empty():
-            event = q.get()
-            arrived_logger.info(f'the file {event.src_path} has been received')
-            file_name: str = event.src_path.split("/")[-1]
-            file_full_name = file_name.split("_")[0].split('.')[0]
-            if "_a" == file_name[-2:] and "_b" == file_name[-2:]:
-                error_logger.error(f'the file {file_name} name is not in the requested format')
-            else:
-                handle_file(file_name, file_full_name)
-
-
 def run():
     observer = Observer()
     watchdog_queue = Queue()
     handle_existing_files(watchdog_queue)
-    worker = Thread(target=process_queue, args=(watchdog_queue,), daemon=True)
+    worker = Process(target=process_queue, args=(watchdog_queue,), daemon=True)
     worker.start()
     event_handler = Handler(watchdog_queue)
     observer.schedule(event_handler, config.IMAGES_DIR_PATH)
@@ -57,6 +45,32 @@ def handle_existing_files(watchdog_queue):
         watchdog_queue.put(event)
 
 
+def process_queue(q):
+    while True:
+        if not q.empty():
+            event = q.get()
+            arrived_logger.info(f'the file {event.src_path} has been received')
+            file_name: str = event.src_path.split("/")[-1]
+            file_full_name = file_name.split("_")[0].split('.')[0]
+            if "_a" == file_name[-2:] and "_b" == file_name[-2:]:
+                error_logger.error(f'the file {file_name} name is not in the requested format')
+            else:
+                handle_file(file_name, file_full_name)
+
+
+def handle_file(file_name, file_full_name):
+    if Redis.check_existence(file_full_name):
+        first_half = Redis.extract(file_full_name)
+        second_half = file_name
+        if second_half != first_half:
+            HTTPLoad.execute([first_half, second_half])
+        else:
+            error_logger.warning(f'the file {file_name} has been sent twice')
+            Redis.load(file_name, file_full_name)
+    else:
+        Redis.load(file_name, file_full_name)
+
+
 def start_multi_processing(q):
     while True:
         try:
@@ -71,16 +85,3 @@ class Handler(FileSystemEventHandler):
 
     def on_closed(self, event):
         self.queue.put(event)
-
-
-def handle_file(file_name, file_full_name):
-    if Redis.check_existence(file_full_name):
-        first_half = Redis.extract(file_full_name)
-        second_half = file_name
-        if second_half != first_half:
-            HTTPLoad.execute([first_half, second_half])
-        else:
-            error_logger.warning(f'the file {file_name} has been sent twice')
-            Redis.load(file_name, file_full_name)
-    else:
-        Redis.load(file_name, file_full_name)
