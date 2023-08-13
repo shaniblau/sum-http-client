ca2756b253d154b77ad608660423655dc3302e25
shaniblau <blaushani02@gmail.com>
2023-08-13 16:14:51 +0300

    changed all code logic because of fucking eitan

--- src/app.py
+++ src/app.py
@@ -17,14 +17,14 @@ from set_logger import extendable_logger
 date = datetime.now().strftime("%d_%m_%Y")
 arrived_logger = extendable_logger(f'{config.LOGS_DIR}/files-arrived/{date}.log', log.INFO)
 
+pool = Pool()
+
 
 def run():
     observer = Observer()
     watchdog_queue = Queue()
-    handle_existing_files(watchdog_queue)
-    with Pool() as pool:
-        pool.apply(func=process_queue, args=(watchdog_queue,))
-    event_handler = Handler(watchdog_queue)
+    handle_existing_files()
+    event_handler = Handler()
     observer.schedule(event_handler, config.IMAGES_DIR_PATH)
     observer.start()
     try:
@@ -32,35 +32,29 @@ def run():
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
+        pool.close()
+    pool.join()
     observer.join()
 
 
-def handle_existing_files(watchdog_queue):
+def handle_existing_files():
     for file in os.listdir(config.IMAGES_DIR_PATH):
         file_path = os.path.join(config.IMAGES_DIR_PATH, file)
         event = FileClosedEvent(file_path)
-        watchdog_queue.put(event)
-
-
-def process_queue(q):
-    while True:
-        try:
-            event = q.get()
-            process_event(event)
-        except queue.Empty:
-            continue
-        except Exception as e:
-            error_logger.error(f'the files were not sent do to: {e}')
+        pool.apply_async(func=process_event, args=(event,))
 
 
 def process_event(event):
-    arrived_logger.info(f'the file {event.src_path} has been received')
-    file_name: str = event.src_path.split("/")[-1]
-    file_full_name = file_name.split("_")[0].split('.')[0]
-    if "_a" == file_name[-2:] and "_b" == file_name[-2:]:
-        error_logger.error(f'the file {file_name} name is not in the requested format')
-    else:
-        handle_file(file_name, file_full_name)
+    try:
+        arrived_logger.info(f'the file {event.src_path} has been received')
+        file_name: str = event.src_path.split("/")[-1]
+        file_full_name = file_name.split("_")[0].split('.')[0]
+        if "_a" == file_name[-2:] and "_b" == file_name[-2:]:
+            error_logger.error(f'the file {file_name} name is not in the requested format')
+        else:
+            handle_file(file_name, file_full_name)
+    except Exception as e:
+        error_logger.error(f'the files were not sent do to: {e}')
 
 
 def handle_file(file_name, file_full_name):
@@ -77,8 +71,5 @@ def handle_file(file_name, file_full_name):
 
 
 class Handler(FileSystemEventHandler):
-    def __init__(self, queue):
-        self.queue = queue
-
     def on_closed(self, event):
-        self.queue.put(event)
+        pool.apply_async(func=process_event, args=(event,))
