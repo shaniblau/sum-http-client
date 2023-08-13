c5e5329b792e0e6cb9e8cff0596f396ebb1faab4
shaniblau <blaushani02@gmail.com>
2023-08-13 18:36:10 +0300

    changed all code logic because of fucking eitan

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -7,7 +7,7 @@ from help_funcs import create_queue
 
 def test_run_should_call_other_funcs(app_fixture, mocker):
     mock_handle_files = mocker.patch('app.handle_existing_files')
-    mock_observer = mocker.patch('app.observer')
+    mock_observer = mocker.patch('app.Observer')
     mocker.patch('app.time.sleep', side_effect=KeyboardInterrupt)
     app_fixture.run()
     mock_handle_files.assert_called_once()
