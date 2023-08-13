76f44471a36c82670204a4d831c4e80dde7edb0a
shaniblau <blaushani02@gmail.com>
2023-08-13 18:37:30 +0300

    changed all code logic because of fucking eitan

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -11,7 +11,7 @@ def test_run_should_call_other_funcs(app_fixture, mocker):
     mocker.patch('app.time.sleep', side_effect=KeyboardInterrupt)
     app_fixture.run()
     mock_handle_files.assert_called_once()
-    mock_observer.schedule.assert_called_with(app_fixture.Handler(), app_fixture.config.IMAGES_DIR_PATH)
+    mock_observer.schedule.assert_called()
 
 
 def test_handle_existing_files_should_create_a_process_for_each_file(app_fixture):
