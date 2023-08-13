8a8f29754b55b49c2d8d54e09d94851bea0fcad6
shaniblau <blaushani02@gmail.com>
2023-08-13 18:41:34 +0300

    changed all code logic because of fucking eitan

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -5,7 +5,7 @@ from app import Handler
 from help_funcs import create_queue
 
 
-def test_run_should_call_other_funcs(app_fixture, mocker):
+def test_run_should_call_handle_existing_files_and_observer(app_fixture, mocker):
     mock_handle_files = mocker.patch('app.handle_existing_files')
     mock_observer = mocker.patch('app.Observer')
     mocker.patch('app.time.sleep', side_effect=KeyboardInterrupt)
@@ -14,5 +14,9 @@ def test_run_should_call_other_funcs(app_fixture, mocker):
     mock_observer.assert_called()
 
 
-def test_handle_existing_files_should_create_a_process_for_each_file(app_fixture):
-    pass
+def test_handle_existing_files_should_create_a_process_for_each_file(app_fixture, mocker, mock_pool):
+    mocker.patch('app.os.listdir', return_value=['file1.txt', 'file2.txt'])
+    mocker.patch('app.pool', mock_pool)
+    app_fixture.handle_existing_files()
+    mock_pool.apply_async.assert_called()
+    assert mock_pool.apply_async.call_count == 2
