96a0663d98528c97748581644982628abe877e2e
shaniblau <blaushani02@gmail.com>
2023-08-13 18:47:09 +0300

    changed all code logic because of fucking eitan

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -14,9 +14,23 @@ def test_run_should_call_handle_existing_files_and_observer(app_fixture, mocker)
     mock_observer.assert_called()
 
 
-def test_handle_existing_files_should_create_a_process_for_each_file(app_fixture, mocker, mock_pool):
-    mocker.patch('app.os.listdir', return_value=['file1.txt', 'file2.txt'])
-    mocker.patch('app.pool', mock_pool)
-    app_fixture.handle_existing_files()
-    mock_pool.apply_async.assert_called()
-    assert mock_pool.apply_async.call_count == 2
+# def test_handle_existing_files_should_create_a_process_for_each_file(app_fixture, mocker, mock_pool):
+#     mocker.patch('app.os.listdir', return_value=['file1.txt', 'file2.txt'])
+#     mocker.patch('app.pool', mock_pool)
+#     app_fixture.handle_existing_files()
+#     mock_pool.apply_async.assert_called()
+#     assert mock_pool.apply_async.call_count == 2
+
+
+def test_process_event(app_fixture, mocker):
+    mock_event = mocker.Mock()
+    mocker.patch('your_module.Redis.check_existence', return_value=True)
+    mocker.patch('your_module.Redis.extract', return_value='first_half')
+    mocker.patch('your_module.HTTPLoad.execute')
+    mocker.patch('your_module.Redis.load')
+    app_fixture.process_event(mock_event)
+    assert app_fixture.Redis.check_existence.called
+    assert app_fixture.Redis.extract.called
+    app_fixture.HTTPLoad.execute.assert_called()
+    app_fixture.Redis.load.assert_called()
+
