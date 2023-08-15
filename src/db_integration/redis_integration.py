849b5c9e2302c1cb7c38512879a6eef217ff310b
shaniblau <blaushani02@gmail.com>
2023-08-15 09:56:24 +0300

    finished all app.py tests

--- src/db_integration/redis_integration.py
+++ src/db_integration/redis_integration.py
@@ -8,16 +8,16 @@ redis_client = RedisClient(host=config.REDIS_HOST, port=config.REDIS_PORT, decod
 
 class Redis(AbstractDB):
     @staticmethod
-    def check_existence(file_full_name):
-        return redis_client.exists(file_full_name)
+    def check_existence(whole_file_name):
+        return redis_client.exists(whole_file_name)
 
     @staticmethod
-    def load(file_name, file_full_name):
-        redis_client.set(file_full_name, file_name)
-        redis_client.expire(file_full_name, config.DEL_TIME)
+    def load(file_name, whole_file_name):
+        redis_client.set(whole_file_name, file_name)
+        redis_client.expire(whole_file_name, config.DEL_TIME)
 
     @staticmethod
-    def extract(file_full_name):
-        first_half = redis_client.get(file_full_name)
-        redis_client.delete(file_full_name)
+    def extract(whole_file_name):
+        first_half = redis_client.get(whole_file_name)
+        redis_client.delete(whole_file_name)
         return first_half
