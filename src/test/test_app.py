2136a7b079449191c5b86630f992ae9654346f65
shaniblau <blaushani02@gmail.com>
2023-08-15 14:42:21 +0300

    finished all app.py tests

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -2,14 +2,14 @@ import logging
 
 
 def test_process_file_invalid_file_name_should_log_error(app_fixture, caplog):
-    expected = 'the file file_c name is not in the requested format'
+    expected = 'the file file_c.txt name is not in the requested format'
     app_fixture.process_file('/file_c.txt')
     error_messages = [record[2] for record in caplog.record_tuples if record[1] == logging.ERROR]
     assert expected in error_messages
 
 
 def test_process_file_invalid_parameter_should_log_error(app_fixture, caplog):
-    expected = "the files were not sent do to: argument of type 'int' is not iterable"
+    expected = "the files were not sent do to: 'int' object has no attribute 'split'"
     app_fixture.process_file(5)
     error_messages = [record[2] for record in caplog.record_tuples if record[1] == logging.ERROR]
     assert expected in error_messages
