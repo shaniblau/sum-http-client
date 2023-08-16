38a945b3cefe0196495e7b637d3f72d411421048
shaniblau <blaushani02@gmail.com>
2023-08-16 16:01:38 +0300

    finished all app.py tests

--- src/test/test_http_load.py
+++ src/test/test_http_load.py
@@ -10,10 +10,10 @@ def test_execute(http_load_fixture, mock_requests, mocker):
     for name in files_names:
         with open(name, 'rb') as file:
             files.append(("files", (name, file.read(), "image/jpg")))
-    mock_create_files = mocker.patch('load.http_load.HTTPLoad__create_files', return_value=files)
-    mock_load_files = mocker.patch('load.http_load.HTTPLoad__load_files', return_value=requests.Response)
-    mock_log_response = mocker.patch('load.http_load.HTTPLoad__log_response')
-    mock_delete_files = mocker.patch('load.http_load.HTTPLoad__delete_files')
+    mock_create_files = mocker.patch('load.http_load.HTTPLoad._HTTPLoad__create_files', return_value=files)
+    mock_load_files = mocker.patch('load.http_load.HTTPLoad._HTTPLoad__load_files', return_value=requests.Response)
+    mock_log_response = mocker.patch('load.http_load.HTTPLoad._HTTPLoad__log_response')
+    mock_delete_files = mocker.patch('load.http_load.HTTPLoad._HTTPLoad__delete_files')
     http_load_fixture.execute(files_names)
     mock_create_files.assert_called_once_with(files_names)
     mock_load_files.assert_called_once_with(files)
