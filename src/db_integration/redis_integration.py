c758fa7666172f71101ce43a842cbfa015a67403
shaniblau <blaushani02@gmail.com>
2023-08-23 19:18:06 +0300

    changed tests according to code changes

--- src/db_integration/redis_integration.py
+++ src/db_integration/redis_integration.py
@@ -2,20 +2,19 @@ from redis import Redis as RedisClient
 from .abstract_db import AbstractDB
 from configuration import config
 
-
 redis_client = RedisClient(host=config.REDIS_HOST, port=config.REDIS_PORT, decode_responses=True)
 
 
 class Redis(AbstractDB):
     @staticmethod
     def check_existence(whole_file_name):
-        return redis_client.exists(whole_file_name)
+        with redis_client.lock('redis_lock') as lock:
+            return redis_client.exists(whole_file_name)
 
     @staticmethod
     def load(file_name, whole_file_name):
-        with redis_client.lock('redis_lock') as lock:
-            redis_client.setnx(whole_file_name, file_name)
-            redis_client.expire(whole_file_name, config.DEL_TIME)
+        redis_client.setnx(whole_file_name, file_name)
+        redis_client.expire(whole_file_name, config.DEL_TIME)
 
     @staticmethod
     def extract(whole_file_name):
