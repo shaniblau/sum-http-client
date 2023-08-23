547e4239505fddc2d8bd62afd44b8fe5b5d2b914
shaniblau <blaushani02@gmail.com>
2023-08-23 18:49:40 +0300

    changed tests according to code changes

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -23,6 +23,7 @@ def test_process_file_should_call_handle_half(app_fixture, mocker):
     mock_handle_half.assert_called_once()
 
 
+
 def test_handle_half_2_identical_files_names(app_fixture, mocker, caplog):
     mocker.patch('app.Redis.check_existence', return_value=True)
     mocker.patch('app.Redis.extract', return_value='file_a.txt')
@@ -32,23 +33,22 @@ def test_handle_half_2_identical_files_names(app_fixture, mocker, caplog):
     error_messages = [record[2] for record in caplog.record_tuples if record[1] == logging.WARNING]
     assert expected == error_messages[0]
     assert len(error_messages) == 1
-    assert mock_load.call_count == 2
-    mock_load.assert_called_with('file_a.txt', 'file')
+    mock_load.assert_called_once_with('file_a.txt', 'file')
 
 
 def test_handle_half_new_file_should_call_redis_load(app_fixture, mocker):
     mock_load = mocker.patch('app.Redis.load')
     mock_extract = mocker.patch('app.Redis.extract')
     app_fixture.handle_half('file_a.txt', 'file')
-    mock_load.assert_called_once_with('file_a.txt', 'file')
+    mock_load.assert_called_once()
     mock_extract.assert_not_called()
 
 
-def test_handle_half_existing_file_should_call_http_load_and_execute(app_fixture, mocker):
+def test_handle_half_existing_file_should_call_http_load_execute(app_fixture, mocker):
     mocker.patch('app.Redis.check_existence', return_value=True)
     mocker.patch('app.Redis.extract', return_value='file_b')
     mock_load = mocker.patch('app.Redis.load')
     mock_execute = mocker.patch('app.HTTPLoad.execute')
     app_fixture.handle_half('file_a.txt', 'file')
-    mock_load.assert_called_once_with('file_a.txt', 'file')
     mock_execute.assert_called_once_with(['file_b', 'file_a.txt'])
+    mock_load.assert_not_called()
--- src/test/test_redis.py
+++ src/test/test_redis.py
@@ -15,9 +15,9 @@ def test_load_should_be_true(redis_fixture, redis_client_fixture):
     assert expected == result
 
 
-# def test_extract_should_be_file_a(redis_fixture, redis_client_fixture):
-#     redis_fixture.load('file_a', 'file.jpg')
-#     result = redis_fixture.extract('file.jpg')
-#     expected = 'file_a'
-#     redis_cleanup(redis_client_fixture, 'file.jpg')
-#     assert result == expected
+def test_extract_should_be_file_a(redis_fixture, redis_client_fixture):
+    redis_fixture.load('file_a', 'file.jpg')
+    result = redis_fixture.extract('file.jpg')
+    expected = 'file_a'
+    redis_cleanup(redis_client_fixture, 'file.jpg')
+    assert result == expected
